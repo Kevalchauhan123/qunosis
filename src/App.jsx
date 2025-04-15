dad7a7202c23d9ab47568060fa0d857b8c47cee9
Keval Chauhan <keval.k.chauhan@gmail.com>
2025-04-15 17:09:16 +0530

    service page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Index from './templates/Index'
 import About from './templates/About';
 import Team from './templates/Team';
+import Service from './templates/Service';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/team" element={<Team />} />
+          <Route path="/services" element={<Service />} />
         </Routes>
       </HashRouter>
     </>
